214b1ba2764b570926cf5cdba648ec48b357234d
Elise Henry <30988198+elisehenry@users.noreply.github.com>
2017-08-17 11:42:42 -0500

    Update header.php

--- header.php
+++ header.php
@@ -23,75 +23,32 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyfifteen' ); ?></a>
 <section class="header">
             <div class="header-content">
-                                                            <a class="logo" href="<?php echo site_url(); ?>" <p><?php bloginfo('name'); ?></p>
+                                                            <a class="logo" href="<?php echo site_url(); ?>" <p class="logo"><?php bloginfo('name'); ?></p>
             </div>
         </section>
 	<div id="content" class="site-content">
 		<style>
-			.header {
-    height: 50px;
-    width: 2000px;
-}
-(index):76
 .header {
-    /* padding: 30px 45px; */
-    background-color: #FFF;
-    border-bottom: 1px solid #f2f2f2;
-    margin-bottom: 50px;
+    height: 50px;
+    width: 3000px;
 }
-(index):91
-user agent stylesheet article, aside, footer, header, hgroup, main, nav, section {
-    display: block;
-}
-			element.style {
-}
-default.less:29
-* {
-    box-sizing: border-box;
-}
-user agent stylesheet
-div {
-    display: block;
-}
-Inherited from body
-default.less:17
-body {
-    background-color: #fbfbfb;
-    font-size: 16px;
-    line-height: 1.8;
-    color: #787c7f;
-    min-width: 350px;
-    font-family: 'Noto Sans';
-    font-weight: 300;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-}
-element.style {
-}
-default.less:41
 .header {
     padding: 30px 45px;
     background-color: #FFF;
     border-bottom: 1px solid #f2f2f2;
     margin-bottom: 50px;
 }
-normalize.less:33
-article, aside, details, figcaption, figure, footer, header, hgroup, main, menu, nav, section, summary {
+div {
     display: block;
 }
-default.less:29
 * {
     box-sizing: border-box;
 }
-user agent stylesheet
 article, aside, footer, header, hgroup, main, nav, section {
     display: block;
 }
-Inherited from body
-default.less:17
 body {
     background-color: #fbfbfb;
     font-size: 16px;
@@ -102,5 +59,25 @@ body {
     font-weight: 300;
     text-rendering: optimizeLegibility;
     -webkit-font-smoothing: antialiased;
+}
+.header {
+    padding: 30px 45px !important;
+    background-color: #FFF !important;
+    border-bottom: 1px solid #f2f2f2 !important;
+    margin-bottom: 50px !important;
+}
+.header #logo {
+    color: #333;
+    display: inline-block;
+    text-decoration: none;
+    height: 50px;
+    width: 300px;
+    background-position: center left;
+    background-size: contain;
+    background-repeat: no-repeat;
+    box-sizing: border-box;
+}
+.header-content {
+    display: block;
 }
 		</style>
